d00696fdd538acf7b6e70fcde3d916994fe772d0
Naoya Yamauchi <wright-ism@live.jp>
2018-04-26 15:43:10 +0900

    初めてのコミット
    カリキュラム途中経過

---
+++ app/src/main/java/com/example/na_sun/taskapp/Task.java
@@ -0,0 +1,47 @@
+package com.example.na_sun.taskapp;
+
+import java.io.Serializable;
+import java.util.Date;
+
+import io.realm.RealmModel;
+import io.realm.RealmObject;
+import io.realm.annotations.PrimaryKey;
+
+public class Task extends RealmObject implements Serializable, RealmModel {
+    private String title;
+    private String contents;
+    private Date date;
+
+    //idをプライマリキーとして設定
+    @PrimaryKey
+    private int id;
+
+    public String getTitle() {
+        return title;
+    }
+    public void setTitle(String title){
+        this.title =title;
+    }
+
+    public String getContents() {
+        return contents;
+    }
+
+    public void setContents(String contents) {
+        this.contents = contents;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date){
+        this.date = date;
+    }
+    public int getId(){
+        return id;
+    }
+    public void setId(int id){
+        this.id = id;
+    }
+}
---
+++ app/src/main/java/com/example/na_sun/taskapp/TaskAlarmReceiver.java
@@ -0,0 +1,14 @@
+package com.example.na_sun.taskapp;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
+
+public class TaskAlarmReceiver extends BroadcastReceiver{
+
+    @Override
+    public void onReceive(Context context, Intent intent ){
+        Log.d("taskapp","onReceive");
+    }
+}
